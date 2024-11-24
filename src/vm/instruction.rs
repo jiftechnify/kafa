2a3f9193b227fd9a98ad3987b596ae80ca2e5b56
jiftechnify <jiftech.stlfy@gmail.com>
2024-11-25 00:25:35 +0900

    add load/store instructions

--- src/vm/instruction.rs
+++ src/vm/instruction.rs
@@ -33,20 +33,67 @@ const INSTRUCTION_TABLE: [Option<Instruction>; 256] = instruction_table! {
     0x07 => instr_iconst::<4>,
     0x08 => instr_iconst::<5>,
     0x09 => instr_lconst::<0>,
-    0x10 => instr_lconst::<1>,
-    0x11 => instr_fconst_0,
-    0x12 => instr_fconst_1,
-    0x13 => instr_fconst_2,
-    0x14 => instr_dconst_0,
-    0x15 => instr_dconst_1,
-    0x16 => instr_bipush,
-    0x17 => instr_sipush,
+    0x0A => instr_lconst::<1>,
+    0x0B => instr_fconst_0,
+    0x0C => instr_fconst_1,
+    0x0D => instr_fconst_2,
+    0x0E => instr_dconst_0,
+    0x0F => instr_dconst_1,
+    0x10 => instr_bipush,
+    0x11 => instr_sipush,
 
-    0x1A => instr_iload::<0>,
-    0x1B => instr_iload::<1>,
-    0x1C => instr_iload::<2>,
-    0x3C => instr_istore::<1>,
-    0x3D => instr_istore::<2>,
+    0x15 => instr_iload,
+    0x16 => instr_lload,
+    0x17 => instr_fload,
+    0x18 => instr_dload,
+    0x19 => instr_aload,
+    0x1A => instr_iload_n::<0>,
+    0x1B => instr_iload_n::<1>,
+    0x1C => instr_iload_n::<2>,
+    0x1D => instr_iload_n::<3>,
+    0x1E => instr_lload_n::<0>,
+    0x1F => instr_lload_n::<1>,
+    0x20 => instr_lload_n::<2>,
+    0x21 => instr_lload_n::<3>,
+    0x22 => instr_fload_n::<0>,
+    0x23 => instr_fload_n::<1>,
+    0x24 => instr_fload_n::<2>,
+    0x25 => instr_fload_n::<3>,
+    0x26 => instr_dload_n::<0>,
+    0x27 => instr_dload_n::<1>,
+    0x28 => instr_dload_n::<2>,
+    0x29 => instr_dload_n::<3>,
+    0x2A => instr_aload_n::<0>,
+    0x2B => instr_aload_n::<1>,
+    0x2C => instr_aload_n::<2>,
+    0x2D => instr_aload_n::<3>,
+
+    0x36 => instr_istore,
+    0x37 => instr_lstore,
+    0x38 => instr_fstore,
+    0x39 => instr_dstore,
+    0x3A => instr_astore,
+    0x3B => instr_istore_n::<0>,
+    0x3C => instr_istore_n::<1>,
+    0x3D => instr_istore_n::<2>,
+    0x3E => instr_istore_n::<3>,
+    0x3F => instr_lstore_n::<0>,
+    0x40 => instr_lstore_n::<1>,
+    0x41 => instr_lstore_n::<2>,
+    0x42 => instr_lstore_n::<3>,
+    0x43 => instr_fstore_n::<0>,
+    0x44 => instr_fstore_n::<1>,
+    0x45 => instr_fstore_n::<2>,
+    0x46 => instr_fstore_n::<3>,
+    0x47 => instr_dstore_n::<0>,
+    0x48 => instr_dstore_n::<1>,
+    0x49 => instr_dstore_n::<2>,
+    0x4A => instr_dstore_n::<3>,
+    0x4B => instr_astore_n::<0>,
+    0x4C => instr_astore_n::<1>,
+    0x4D => instr_astore_n::<2>,
+    0x4E => instr_astore_n::<3>,
+
     0x60 => instr_iadd,
     0x84 => instr_iinc,
     0xA3 => instr_if_icmpgt,
@@ -112,20 +159,61 @@ fn instr_sipush(t: &mut Thread) -> InstructionResult {
     Ok(())
 }
 
-// push the Nth local to the operand stack
-fn instr_iload<const N: usize>(t: &mut Thread) -> InstructionResult {
-    let frame = t.current_frame();
-    frame.push_operand(frame.get_local(N));
-    Ok(())
+// push the specified local (by index) to the operand stack
+macro_rules! instr_load {
+    ($name:ident, $name_n:ident, $vtype:path, $vtype_name:expr) => {
+        fn $name(t: &mut Thread) -> InstructionResult {
+            let frame = t.current_frame();
+            let idx = frame.next_param_u8() as usize;
+            let v @ $vtype(_) = frame.get_local(idx) else {
+                return Err(concat!("target local is not type '", $vtype_name, "'").into());
+            };
+            frame.push_operand(v);
+            Ok(())
+        }
+        fn $name_n<const N: usize>(t: &mut Thread) -> InstructionResult {
+            let frame = t.current_frame();
+            let v @ $vtype(_) = frame.get_local(N) else {
+                return Err(concat!("target local is not type '", $vtype_name, "'").into());
+            };
+            frame.push_operand(v);
+            Ok(())
+        }
+    };
 }
+instr_load!(instr_iload, instr_iload_n, Value::Int, "int");
+instr_load!(instr_lload, instr_lload_n, Value::Long, "long");
+instr_load!(instr_fload, instr_fload_n, Value::Float, "float");
+instr_load!(instr_dload, instr_dload_n, Value::Double, "double");
+instr_load!(instr_aload, instr_aload_n, Value::Reference, "reference");
 
-// pop from the operand stack and store it to the Nth local
-fn instr_istore<const N: usize>(t: &mut Thread) -> InstructionResult {
-    let frame = t.current_frame();
-    let v = frame.pop_operand();
-    frame.set_local(N, v);
-    Ok(())
+// pop from the operand stack and store it to the specified local (by index)
+macro_rules! instr_store {
+    ($name:ident, $name_n:ident, $vtype:path, $vtype_name:expr) => {
+        fn $name(t: &mut Thread) -> InstructionResult {
+            let frame = t.current_frame();
+            let idx = frame.next_param_u8() as usize;
+            let v @ $vtype(_) = frame.pop_operand() else {
+                return Err(concat!("target operand is not type '", $vtype_name, "'").into());
+            };
+            frame.set_local(idx, v);
+            Ok(())
+        }
+        fn $name_n<const N: usize>(t: &mut Thread) -> InstructionResult {
+            let frame = t.current_frame();
+            let v @ $vtype(_) = frame.pop_operand() else {
+                return Err(concat!("target operand is not type '", $vtype_name, "'").into());
+            };
+            frame.set_local(N, v);
+            Ok(())
+        }
+    };
 }
+instr_store!(instr_istore, instr_istore_n, Value::Int, "int");
+instr_store!(instr_lstore, instr_lstore_n, Value::Long, "long");
+instr_store!(instr_fstore, instr_fstore_n, Value::Float, "float");
+instr_store!(instr_dstore, instr_dstore_n, Value::Double, "double");
+instr_store!(instr_astore, instr_astore_n, Value::Reference, "reference");
 
 // pop 2 values, add them and push the result
 fn instr_iadd(t: &mut Thread) -> InstructionResult {
